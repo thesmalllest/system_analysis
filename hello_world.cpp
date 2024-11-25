4c1f6b38aed49dff338c8208120ec0b2492fc366
thesmalllest <vikulik.vs.137@gmail.com>
2024-11-25 13:30:33 +0300

    Удаляем using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-using namespace std;
 
 int main(){
-	string name;
-	cout<<"Username:";
-	cin>>name;
-	cout<<"Hello World from "<<name<<"!"<<endl;
+	std::string name;
+	std::cout<<"Username:";
+	std::cin>>name;
+	std::cout<<"Hello World from "<<name<<"!"<<endl;
 	return 0;
 }
 
